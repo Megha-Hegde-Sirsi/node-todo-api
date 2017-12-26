e909a2b620c9b5ee8567c58b8c49f331624ba3c8
Megha-Hegde-Sirsi <meghahegde30@gmail.com>
2017-12-26 15:37:16 +0530

    copied

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -1,42 +1,34 @@
 // const MongoClient = require('mongodb').MongoClient;
-const { MongoClient, ObjectId } = require('mongodb');
+const { MongoClient, ObjectID } = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
-    if (err) {
-        return console.log('Unable to connect to mongoDB');
-    }
-    console.log('Connected to mongoDB server');
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
 
-    //deleteMany
-    // db.collection('Todos').deleteMany({
-    //     age: 22
-    // }).then((result) => {
-    //     console.log(result);
-    // });
-    //deleteOne
-    // db.collection('Todos').deleteOne({
-    //     age: 22
-    // }).then((result) => {
-    //     console.log(result);
-    // });
-    //findOneAndDelete
-    // db.collection('Todos').findOneAndDelete({
-    //     name: 'Lasya'
-    // }).then((result) => {
-    //     console.log(result);
-    // });
+  // deleteMany
+  // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
 
-    db.collection('Users').deleteMany({
-        test: 'Nothing leave'
-    }).then((result)=>{
-        console.log(result);
-    });
+  // deleteOne
+  // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
 
-    // db.collection('Users').findOneAndDelete({
-    //     _id: new ObjectId('5a3b5a3ee0dcd81541173111')
-    // }).then((result)=>{
-    //     console.log(result);
-    // });
+  // findOneAndDelete
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+  //   console.log(result);
+  // });
 
-    //db.close();
+  // db.collection('Users').deleteMany({name: 'Andrew'});
+
+  db.collection('Users').findOneAndDelete({
+    _id: new ObjectID("57ac8d47878a299e5dc21bc8")
+  }).then((results) => {
+    console.log(JSON.stringify(results, undefined, 2));
+  });
+
+  // db.close();
 });
