647ee95c6ba4f0cdbb44d68ddc83ceaf77111e97
Megha-Hegde-Sirsi <meghahegde30@gmail.com>
2017-12-28 12:42:30 +0530

    hash user password

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -13,7 +14,7 @@ var UserSchema = new mongoose.Schema({
     validate: {
       isAsync: false,
       validator: validator.isEmail,
-      
+
       message: '{VALUE} is not a valid email'
     }
   },
@@ -48,7 +49,7 @@ UserSchema.methods.generateAuthToken = function () {
 
   // user.tokens.push({ access, token });
 
-user.tokens = user.tokens.concat([{access, token}]);
+  user.tokens = user.tokens.concat([{ access, token }]);
 
   return user.save().then(() => {
     return token;
@@ -71,6 +72,24 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.access': 'auth'
   });
 };
+
+UserSchema.pre('save', function (next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    //user.password
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+    //user.password
+    //next()
+  } else {
+    next();
+  }
+});
 
 var User = mongoose.model('User', UserSchema);
 
