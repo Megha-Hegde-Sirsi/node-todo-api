bdb1f111c81a09827d31bea371af1eaef8b8f119
Megha-Hegde-Sirsi <meghahegde30@gmail.com>
2017-12-28 18:06:55 +0530

    login

--- server/models/user.js
+++ server/models/user.js
@@ -12,9 +12,7 @@ var UserSchema = new mongoose.Schema({
     minlength: 1,
     unique: true,
     validate: {
-      isAsync: false,
       validator: validator.isEmail,
-
       message: '{VALUE} is not a valid email'
     }
   },
@@ -47,9 +45,7 @@ UserSchema.methods.generateAuthToken = function () {
   var access = 'auth';
   var token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
 
-  // user.tokens.push({ access, token });
-
-  user.tokens = user.tokens.concat([{ access, token }]);
+  user.tokens.push({ access, token });
 
   return user.save().then(() => {
     return token;
@@ -73,19 +69,37 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({ email }).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      // Use bcrypt.compare to compare password and user.password
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
 UserSchema.pre('save', function (next) {
   var user = this;
 
   if (user.isModified('password')) {
-    //user.password
     bcrypt.genSalt(10, (err, salt) => {
       bcrypt.hash(user.password, salt, (err, hash) => {
         user.password = hash;
         next();
       });
     });
-    //user.password
-    //next()
   } else {
     next();
   }
@@ -93,4 +107,4 @@ UserSchema.pre('save', function (next) {
 
 var User = mongoose.model('User', UserSchema);
 
-module.exports = { User };
+module.exports = { User }
