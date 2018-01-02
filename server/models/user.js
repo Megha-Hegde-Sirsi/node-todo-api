5a19ad5c5403676b2f418d89873558fbc392d241
Megha-Hegde-Sirsi <meghahegde30@gmail.com>
2018-01-02 11:22:13 +0530

    todo api is done

--- server/models/user.js
+++ server/models/user.js
@@ -43,12 +43,10 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {
   var user = this;
   var access = 'auth';
-  var token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
+  var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
-  // user.tokens.push({ access, token });
-
-  user.tokens = user.tokens.concat([{ access, token }]);
-
+ // user.tokens.push({access, token});
+ user.tokens = user.tokens.concat([{access, token}]);
   return user.save().then(() => {
     return token;
   });
@@ -59,7 +57,7 @@ UserSchema.methods.removeToken = function (token) {
 
   return user.update({
     $pull: {
-      tokens: { token }
+      tokens: {token}
     }
   });
 };
@@ -69,7 +67,7 @@ UserSchema.statics.findByToken = function (token) {
   var decoded;
 
   try {
-    decoded = jwt.verify(token, 'abc123');
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (e) {
     return Promise.reject();
   }
@@ -84,7 +82,7 @@ UserSchema.statics.findByToken = function (token) {
 UserSchema.statics.findByCredentials = function (email, password) {
   var User = this;
 
-  return User.findOne({ email }).then((user) => {
+  return User.findOne({email}).then((user) => {
     if (!user) {
       return Promise.reject();
     }
@@ -119,4 +117,4 @@ UserSchema.pre('save', function (next) {
 
 var User = mongoose.model('User', UserSchema);
 
-module.exports = { User }
+module.exports = {User}
